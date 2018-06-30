e17b908cb15146949e28c37d54b680c12c6edf46
Radek Pištělák <radek.pistelak@gmail.com>
2018-06-30 20:00:30 +0200

    Initial commit

---
+++ NSObjectThrottling/NSObjectThrottling.h
@@ -0,0 +1,17 @@
+//
+//  NSObjectThrottling.h
+//  NSObjectThrottling
+//
+//  Created by Radek Pistelak on 30/06/2018.
+//  Copyright © 2018 ran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSObjectThrottling.
+FOUNDATION_EXPORT double NSObjectThrottlingVersionNumber;
+
+//! Project version string for NSObjectThrottling.
+FOUNDATION_EXPORT const unsigned char NSObjectThrottlingVersionString[];
+
+#import <NSObjectThrottling/NSObject+Throttling.h>
